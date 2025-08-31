899684e21305f7fb86056cfa361986f18e51d739
SMahiieee <mahisharmaprr@gmail.com>
2025-08-31 23:25:38 +0530

    Add files via upload

---
+++ Q6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+    printf("Enter the first number (a): ");
+    scanf("%d", &a);
+    printf("Enter the second number (b): ");
+    scanf("%d", &b);
+    temp = a;
+    a = b;
+    b = temp;
+    printf("After swapping:\n");
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    return 0;
+}
